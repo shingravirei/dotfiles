942b36f678332fb57544b73b486aad37ea9973ae
Victor <zedfromionia@gmail.com>
2020-05-05 19:18:02 -0300

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    trailingComma: 'none',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    useTabs: false,
+    bracketSpacing: true,
+    arrowParens: "avoid"
+};
